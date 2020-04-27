6d589addbec380d8cb312e3d9b3ebf01bb3674c4
Andy Contreras <andy.j.contreras@me.com>
2020-04-26 22:08:08 -0500

    header comp

---
+++ src/Components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let bgColor = '#56ac89'
+</script>
+
+<header style="{`background: ${bgColor};`}">
+    <h1><i class="fas fa-frog"></i> Irogero</h1> 
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: start;
+        align-items: baseline;
+        color: azure; 
+        padding: 1em 2em;
+        transition: background-color 400ms linear;
+    }
+
+    h1, p {
+        margin: 0;
+        margin-right: .4em;
+    }
+</style>
