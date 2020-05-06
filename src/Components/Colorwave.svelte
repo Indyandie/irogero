c4504f55ff4dad7679acd4dd6f62dd86d44855b6
Andy Contreras <andy.j.contreras@me.com>
2020-05-06 00:01:25 -0500

    wave select

---
+++ src/Components/Colorwave.svelte
@@ -0,0 +1,54 @@
+<script>
+    export let selectedWave = 'easeInCubic'
+    let waveArray = [
+        'linear', 
+
+        'easeInCubic', 
+        'easeOutCubic', 
+        'easeInOutCubic',
+
+        'easeInSine',
+        'easeOutSine',
+        'easeInOutSine',
+
+        'easeInQuad',
+        'easeOutQuad',
+        'easeInOutQuad',
+
+        'easeInQuart',
+        'easeOutQuart',
+        'easeInOutQuart',
+
+        'easeInCirc',
+        'easeOutCirc',
+        'easeInOutCirc',
+
+        'easeInQuint',
+        'easeOutQuint',
+        'easeInOutQuint',
+
+        'easeInExpo',
+        'easeOutExpo',
+        'easeInOutExpo',
+
+        'easeInBack',
+        'easeOutBack',
+        'easeInOutBack',
+    ]
+</script>
+
+<select name="wave" bind:value={selectedWave}>
+    {#each waveArray as item}
+    <option value={item}>{item}</option>    
+    {/each}
+</select>
+
+<style>
+    select {
+        min-width: 180px;
+        padding: 0.75em;
+        margin-bottom: 2em;
+        border: 2px solid #444;
+    }
+</style>
+
