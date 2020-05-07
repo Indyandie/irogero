28e9f6e9d91111bc1ec71803e2f04cd6b11cf828
Andy Contreras <andy.j.contreras@me.com>
2020-05-07 00:39:11 -0500

    simplify

--- src/Components/ColorWell.svelte
+++ src/Components/ColorWell.svelte
@@ -4,7 +4,7 @@
 </script>
 
 
-<code>{wellColor}</code>    
+<!-- <code>{wellColor}</code>     -->
 <input class:margin type="color" bind:value={wellColor} name="selectedColor" id="colorWell">
 <!-- <label for="colorWell">HEX Color</label><br> -->
 
@@ -18,21 +18,23 @@
         -webkit-appearance: none;
         outline: none;
         border: none;
-        width: 7em;
-        height: 7em;
-        border: 4px solid #444;
-        border-radius: 100%;
+        width: 120px;
+        height: 3em;
+        /* width: 7em;
+        height: 7em; */
+        /* border: 4px solid #444;
+        border-radius: 100%; */
         background: none;
         transition: background-color 300ms linear;
     }
 
-    input[type="color"]::-webkit-color-swatch-wrapper {
+    /* input[type="color"]::-webkit-color-swatch-wrapper {
         padding: 0;
-    }
-    input[type="color"]::-webkit-color-swatch {
-        border: solid 1px #000; /*change color of the swatch border here*/
+    } */
+    /* input[type="color"]::-webkit-color-swatch {
+        border: solid 1px #000; 
         border-radius: 40px;
-    }
+        } */
 
     .margin {
         margin-bottom: 2em;
