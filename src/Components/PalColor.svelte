bd3b94442bb0df69a3bc0afc360c5a1487202260
Andy Contreras <andy.j.contreras@me.com>
2020-05-10 14:47:40 -0500

    add copy hex functionality

--- src/Components/PalColor.svelte
+++ src/Components/PalColor.svelte
@@ -1,17 +1,37 @@
 <script>
-    import { fade } from 'svelte/transition'
+    import { fade, fly } from 'svelte/transition'
+    import copy from 'copy-to-clipboard'
 
     export let textColor = '#ffffff'
     export let colorName = 'name'
     export let colorHex = '#00ff00'
+
+    let visible = false
+
+    function testing() {
+        copy(colorHex)
+        visible = true
+        setTimeout(() => {
+            visible = false
+        }, 777)
+    }
+    
 </script>
 
 
-<div  class="colorSamples" style="background:{colorHex}">
+<div on:click={testing} class="colorSamples" style="background:{colorHex}">
     <div class="colour" style={`color: ${textColor}`} >
         {colorHex.substr(1)}
     </div>
+
+    {#if visible}
+    <div transition:fly id="notification">
+        <i class="fas fa-hand-scissors"></i> copied
+    </div>
+    {/if}
+
 </div>
+
 
 
 <style>
@@ -38,10 +58,32 @@
         transition: all 300ms linear;
     }
 
+    .colorSamples:active {
+        background: black !important;
+        
+    }
+
+    .colour:active {
+        color: white !important;
+    }
+
     .colorPal {
         display: flex;
         justify-content: start;
         flex-wrap: wrap;
+    }
+
+    #notification {
+        position: absolute;
+        bottom: 1vh;
+        border-radius: 1em;
+        width: initial;
+        padding: 1em;
+        background: black;
+        color: white;
+        font-family: 'Tofino', Courier, monospace;
+        font-size: 0.5em;
+        text-transform: capitalize;
     }
 
 </style>
